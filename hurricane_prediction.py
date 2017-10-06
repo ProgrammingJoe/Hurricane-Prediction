5c66a3c90e1e8b2fcf59f3921d1972e48b9614f9
Programmingjoe <joeczepil@gmail.com>
2017-10-06 12:08:42 -0700

    Create list of pairs of rows for training, convert to int and float

--- hurricane_prediction.py
+++ hurricane_prediction.py
@@ -1,4 +1,5 @@
 from csv import DictReader
+import re
 
 class HurricancePrediction:
 
@@ -8,7 +9,6 @@ class HurricancePrediction:
             hurricanes_dict = {}
             current_id = 0
             new_hurricane = []
-            hurricane_count = 1
             for row in r:
                 if current_id == row['ID']:
                     new_hurricane.append({
@@ -21,7 +21,6 @@ class HurricancePrediction:
                 else:
                     hurricanes_dict[current_id] = new_hurricane
                     current_id = row['ID']
-                    hurricane_count += 1
                     new_hurricane = [{
                         'Date': row['Date'],
                         'Time': row['Time'],
@@ -32,11 +31,43 @@ class HurricancePrediction:
 
             return hurricanes_dict
 
+    def merge_rows(self, hurricanes):
+        data = []
+        for key in hurricanes:
+            hurricane = hurricanes[key]
+            for row in range(0, len(hurricane) - 1):
+                if hurricane[row+1]:
+                    data.append([int(hurricane[row]['Date']),
+                        int(hurricane[row]['Time']),
+                        self._lattoint(hurricane[row]['Lat']),
+                        self._longtoint(hurricane[row]['Long']),
+                        int(hurricane[row]['MaxWind']),
+                        int(hurricane[row + 1]['Date']),
+                        int(hurricane[row + 1]['Time'])
+                    ])
+        return data
+
+    def _lattoint(self, latitude):
+        if 'N' in latitude:
+            return float(latitude[:-1]) + 90
+        elif 'S' in latitude:
+            return 90 - float(latitude[:-1])
+
+    def _longtoint(self, longitude):
+        if 'W' in longitude:
+            return float(longitude[:-1]) + 180
+        elif 'E' in longitude:
+            return 180 - float(longitude[:-1])
+
 def main():
     predictor = HurricancePrediction()
 
     pacific_hurricanes = predictor.read('data/pacific.csv')
     atlantic_hurricanes = predictor.read('data/atlantic.csv')
+
+    pacific_training = predictor.merge_rows(pacific_hurricanes)
+    print(pacific_training[0])
+    print(pacific_training[1])
 
 
 if __name__ == "__main__":
